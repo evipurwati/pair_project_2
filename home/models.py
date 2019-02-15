ee68c8db2f8ac6b11bc925a0c77edce8dce318b0
AnggaDAR <angga@alphatech.id>
2019-02-15 10:59:59 +0700

    update home

--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    posted_at = models.DateTimeField(default=timezone.now)
+    total_likes = models.PositiveIntegerField(default=0)
+    total_comments = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='PostImage')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
