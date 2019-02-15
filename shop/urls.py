9c03339fc3f73a3098e9ee36beb38574e09f6cae
AnggaDAR <angga@alphatech.id>
2019-02-15 13:05:06 +0700

    update in release

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.shop, name='shop'),
+    path('shop', views.shop, name='shop'),
     # path('form', views.input_post, name='input_post'),
     path('shop/<int:stuff_id>', views.detail_stuff, name='detail_stuff'),
     path('category/<int:category_id>', views.show_by_category, name='show_by_category'),
