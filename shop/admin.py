89054cca29a49c5f7106fd52833e7b802db92057
evipurwati <evi@alphatech.id>
2019-02-15 10:48:52 +0700

    shop fix

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin        
-from .models import Stuff
+from .models import Stuff, Category
 
-my_model = [Stuff]
+my_model = [Stuff, Category]
 admin.site.register(my_model)
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models import Stuff
+from .models import Stuff, Category
 # from .form import PostStuff
 
 def shop(request) :
+    category = Category.objects.all()
     stuffs = Stuff.objects.all()
-    return render(request, 'shop.html', {'stuffs' : stuffs})
+    return render(request, 'shop.html', {'stuffs' : stuffs, 'category':category})
 
 # def stuff(request) :
 #     stuff = StuffPost.objects.all()
@@ -21,5 +22,11 @@ def shop(request) :
 #     return render(request, 'shop/shop.html', {'form' : form})
 
 def detail_stuff(request, stuff_id) :
+    category = Category.objects.all()
     stuffs = Stuff.objects.get(pk=stuff_id)
-    return render(request, 'detail_stuff.html', {'stuff': stuffs})
+    return render(request, 'detail_stuff.html', {'stuff': stuffs, 'category':category})
+
+def show_by_category(request, category_id) :
+    category = Category.objects.all()
+    stuffs = Stuff.objects.filter(kategori=category_id)
+    return render(request, 'show_by_category.html', {'stuffs': stuffs, 'category':category})
